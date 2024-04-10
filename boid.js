f72fd031cc4f9fb9fa34b64216fc2ecad6c10274
Nathan <nathan.smith1922@gmail.com>
2024-04-11 00:13:33 +1000

    Implemented Separation

--- boid.js
+++ boid.js
@@ -136,14 +136,69 @@ class Boid {
     return steering;
   }
 
+  separation(boids) {
+    let steering = {
+      x: 0,
+      y: 0,
+    };
+    let total = 0;
+    for (let boid of boids) {
+      if (boid == this) {
+        continue;
+      }
+      if (
+        Math.abs(this.position.x - boid.position.x) <= VIEW_RADIUS / 2 &&
+        Math.abs(this.position.y - boid.position.y) <= VIEW_RADIUS / 2
+      ) {
+        let diff = {
+          x: this.position.x - boid.position.x,
+          y: this.position.y - boid.position.y,
+        };
+        let dist =
+          Math.abs(this.position.x - boid.position.x) +
+          Math.abs(this.position.y - boid.position.y);
+        dist /= 2;
+        diff.x /= dist;
+        diff.y /= dist;
+
+        steering.x += diff.x;
+        steering.y += diff.y;
+        total++;
+      }
+    }
+
+    if (total) {
+      steering.x /= total;
+      steering.y /= total;
+
+      let mag = Math.sqrt(steering.x ** 2 + steering.y ** 2);
+      if (steering.x !== 0 && steering.y !== 0) {
+        steering.x *= MAX_VELOCITY / mag;
+        steering.y *= MAX_VELOCITY / mag;
+      }
+
+      steering.x -= this.velocity.x;
+      steering.y -= this.velocity.y;
+
+      let f = Math.min(mag, MAX_FORCE) / mag;
+      if (steering.x !== 0 && steering.y !== 0) {
+        steering.x *= f;
+        steering.y *= f;
+      }
+    }
+
+    return steering;
+  }
+
   steer(boids) {
     this.acceleration.x = 0;
     this.acceleration.y = 0;
 
     let alignment = this.align(boids);
     let cohesion = this.cohesion(boids);
-    this.acceleration.x += cohesion.x + alignment.x;
-    this.acceleration.y = cohesion.y + alignment.y;
+    let separation = this.separation(boids);
+    this.acceleration.x += cohesion.x + alignment.x + separation.x;
+    this.acceleration.y = cohesion.y + alignment.y + separation.y;
   }
 
   update() {
